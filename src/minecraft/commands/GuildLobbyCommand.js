43818b43d8425ba9b116d6cdf28dd703e6435ec0
ReedeB <106803732+ReedeB@users.noreply.github.com>
2023-01-07 13:44:41 -0600

    Changed to Frag Run

--- src/minecraft/commands/GuildLobbyCommand.js
+++ src/minecraft/commands/GuildLobbyCommand.js
@@ -4,13 +4,21 @@ class GuildLobbyCommand extends MinecraftCommand {
   constructor(minecraft) {
     super(minecraft)
 
-    this.name = 'guildlobby'
-    this.aliases = ['globby']
-    this.description = "Whispers user's username to a guild lobby account"
+    this.name = 'fragrun'
+    this.aliases = ['fr']
+    this.description = "why are you here"
   }
 
-  onCommand(username, message) {
-    this.send(`/w ${this.minecraft.app.config.minecraft.lobbyHolder} ?tw ${username}`)
+  onCommand(username, _message) {
+    let text = "godlike cheese";
+    this.send(`/p ${username} ;`)
+    var e = new Date().getTime() + (2 * 1000);
+    while (new Date().getTime() <= e) {}
+    this.send(`/p transfer ${username}`)
+    var s = new Date().getTime() + (10 * 1000);
+    while (new Date().getTime() <= s) {}
+    this.send(`/p leave`)
+
   }
 }
 
